4d7bce402111bf052e4c6d64f432516d7c2d516d
Ganesh <ganeshgopi2612@gmail.com>
2025-04-09 10:31:15 +0530

    [Ganesh] add : Creating classes and objects

---
+++ Day2/Oops Concepts/class_objects.js
@@ -0,0 +1,13 @@
+class Student{
+    constructor(name,age,marks){
+        this.name=name;
+        this.age=age,
+        this.marks=marks,
+        this.batch=2025
+    }
+    department=(dept)=>console.log(`You are from ${dept} department`);
+}
+
+const stud1=new Student("Ganesh",21,9);
+console.log(stud1.name+" "+stud1.age+" "+stud1.marks+" "+stud1.batch);
+stud1.department("CSE");
