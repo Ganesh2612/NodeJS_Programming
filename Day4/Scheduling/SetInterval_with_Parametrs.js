4a49ce2c26e65ba1c1b493150840ba8ce438afbe
Ganesh <ganeshgopi2612@gmail.com>
2025-04-12 11:31:46 +0530

    [Ganesh] add : setinterval with parameters in javascript

---
+++ Day4/Scheduling/SetInterval_with_Parametrs.js
@@ -0,0 +1,5 @@
+function family(father,mother,sister,yourself){
+    console.log("Father is "+father+", Mother is "+mother+', Sister is '+sister+", Yourself is "+yourself);
+}
+
+setInterval(family,3000,"Gopi","Kala","Manasha","Ganesh");
