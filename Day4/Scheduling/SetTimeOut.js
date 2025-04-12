7c341722ffbcbef380dd523ae028929e10a1e0a1
Ganesh <ganeshgopi2612@gmail.com>
2025-04-12 10:27:47 +0530

    [Ganesh] add : SetTimeout code in javascript

---
+++ Day4/Scheduling/SetTimeOut.js
@@ -0,0 +1,11 @@
+function Scheduling(){
+    let name="Ganesh";
+    let age=21;
+    console.log("Name is :"+name+" and age is :"+age);
+}
+
+let schedule=setTimeout(Scheduling,3000);
+console.log("See the delay");
+
+
+
