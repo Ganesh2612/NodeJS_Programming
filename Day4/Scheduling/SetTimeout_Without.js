cff8961b539dbcf054c0964405a7575913db1532
Ganesh <ganeshgopi2612@gmail.com>
2025-04-12 10:39:29 +0530

    [Ganesh] add : setTimeout with passing parameters in it

---
+++ Day4/Scheduling/SetTimeout_Without.js
@@ -0,0 +1,6 @@
+function subject(high,low){
+    console.log("Your son has got highest marks in "+high+"and lowest in "+low);
+}
+let parameter1="Maths";
+let parameter2="Science";
+let answer=setTimeout(subject,3000,parameter1,parameter2);
