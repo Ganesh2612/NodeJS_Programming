866a9b0fdb1df0d85353766129ed66192ec593f8
Ganesh <ganeshgopi2612@gmail.com>
2025-04-15 15:10:28 +0530

    [Ganesh] add : Switch case usage

---
+++ Day5/Switch_case.js
@@ -0,0 +1,11 @@
+let day = "Monday";
+switch(day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Friday":
+    console.log("Almost weekend");
+    break;
+  default:
+    console.log("Just another day");
+}
